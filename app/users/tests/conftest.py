d40f7aae81c64e5d74e1b2c8108f8ad6b8c2c831
PersonalPhoenix <applmacter@mail.ru>
2025-02-18 14:48:10 +0300

    Корректировка фикстуры получения клиента

--- app/users/tests/conftest.py
+++ app/users/tests/conftest.py
@@ -15,7 +15,7 @@ from app.config import (
 
 @pytest.mark.anyio
 @pytest.fixture
-async def async_animals_client():
+async def async_animals_client_api_v1():
     base_url = settings.TEST_URL + settings.DEFAULT_URL_PREFIX_API_V1
     async with AsyncClient(transport=ASGITransport(app=app), base_url=base_url) as async_animals_client:
         yield async_animals_client
