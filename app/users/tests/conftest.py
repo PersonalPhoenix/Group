ec23e52106cb6f687f1226688a0beb8b3ab22a55
PersonalPhoenix <applmacter@mail.ru>
2025-02-18 15:32:27 +0300

    GROUP-1. корректировка нейминга в фикстуре

--- app/users/tests/conftest.py
+++ app/users/tests/conftest.py
@@ -17,8 +17,8 @@ from app.config import (
 @pytest.fixture
 async def async_client_api_v1():
     base_url = settings.TEST_URL + settings.DEFAULT_URL_PREFIX_API_V1
-    async with AsyncClient(transport=ASGITransport(app=app), base_url=base_url) as async_animals_client:
-        yield async_animals_client
+    async with AsyncClient(transport=ASGITransport(app=app), base_url=base_url) as async_client_api_v1:
+        yield async_client_api_v1
 
 
 @pytest.mark.anyio
